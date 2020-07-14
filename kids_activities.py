88aa1dd7cfd27e2006d9ba2431c7a90fee4b1e53
Kirk Sudduth <kirksudduth@gmail.com>
2020-07-14 14:45:29 -0500

    got all those functions printing what i wanted:

---
+++ kids_activities.py
@@ -0,0 +1,24 @@
+def run(*kids):
+    for kid in kids:
+        print(f'{kid} ran like a fool!')
+
+
+def swing(*kids):
+    for kid in kids:
+        print(f'{kid} swung to the stars!')
+
+
+def slide(*kids):
+    for kid in kids:
+        print(f'{kid} slid faster than a penguin into water!')
+
+
+def hide_and_seek(*kids):
+    for kid in kids:
+        print(f'{kid} hid better than a chameleon at night!')
+
+
+run("Pam", "Sam", "Andrea", "Will")
+swing("Marybeth", "Ariel", "Kevin", "Courtney")
+slide("Mike", "Jack", "Jennifer", "Earl")
+hide_and_seek("Henry", "Heather", "Hayley", "Hugh")
